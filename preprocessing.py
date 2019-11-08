8c9b73413628a2e99858cff9f9d86a713cb3c6e8
Akshdeep Rungta <asr@ccs.neu.edu>
2019-11-08 17:10:33 -0500

    Remove print

--- preprocessing.py
+++ preprocessing.py
@@ -6,21 +6,22 @@ from nltk.corpus import stopwords
 
 nltk.download('wordnet')
 nltk.download('stopwords')
-  
+
+
 class DataSet():
     def __init__(self, name="train", path="./FNC-1"):
         self.path = path
 
         print("Reading dataset")
-        bodies = name+"_bodies.csv"
-        stances = name+"_stances.csv"
+        bodies = name + "_bodies.csv"
+        stances = name + "_stances.csv"
 
         self.old_stances = self.read(stances)
         articles = self.read(bodies)
         self.articles = dict()
         self.headlines = dict()
         self.stances = dict()
- 
+
         # make the body ID an integer value
         for s in self.old_stances:
             s['Body ID'] = int(s['Body ID'])
@@ -37,8 +38,7 @@ class DataSet():
         print("Total stances: " + str(len(self.old_stances)))
         print("Total bodies: " + str(len(self.articles)))
 
-
-    def read(self,filename):
+    def read(self, filename):
         rows = []
         with open(self.path + "/" + filename, "r", encoding='utf-8') as table:
             r = DictReader(table)
@@ -47,15 +47,14 @@ class DataSet():
                 rows.append(line)
         return rows
 
-    
     def preprocess(self, lemmatize=True, removeStop=True):
-        lemmatizer = WordNetLemmatizer() 
+        lemmatizer = WordNetLemmatizer()
         data = "All work and no play makes jack dull boy. All work and no play makes jack a dull boy."
         stopWords = set(stopwords.words('english'))
 
         data = {'ID': [], 'Headline': [], 'Body': [], 'Stance': []}
 
-        #self.headlines_lemmatized = pd.
+        # self.headlines_lemmatized = pd.
         for bodyID in self.articles.keys():
             stance = self.stances[bodyID].lower()
             body = self.articles[bodyID].lower()
@@ -79,9 +78,4 @@ class DataSet():
 
             data['ID'].append(bodyID)
 
-        df = pd.DataFrame(data)
-        self.df = df
-        return df
-
-ds = DataSet()
-print(ds.preprocess(False, False)["Body"])
+        return pd.DataFrame(data)
