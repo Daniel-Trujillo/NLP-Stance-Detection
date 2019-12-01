5db58faefa8352e68cefbf4edc0f24815dfb4a26
Daniel <d.trujillo@student.vu.nl>
2019-11-30 21:02:01 -0500

    started TF-IDF

---
+++ SVD/SVD.py
@@ -0,0 +1,65 @@
+from gensim.models import KeyedVectors
+import sklearn
+import pickle
+from preprocessing import DataSet
+import os
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+COSINE_SIMILARITY_FILE = "../feature_files/similarity_features.pkl"
+COSINE_SIMILARITY_TEST_FILE = "../feature_files/similarity_test_features.pkl"
+
+
+class SVD:
+    def __init__(self, name="train", path="../FNC-1", lemmatize=True, remove_stop=True, remove_punc=False):
+        self.path = path
+        self.name = name
+        self.lemmatize = lemmatize
+        self.remove_stop = remove_stop
+        self.remove_punc = remove_punc
+
+    def get_feature(self):
+        ds = DataSet(self.name, self.path)
+        data = ds.preprocess(self.lemmatize, self.remove_stop, self.remove_punc)
+
+        headlines = data.Headline.to_numpy()
+        bodies = data.Body.to_numpy()
+
+        vocabulary = [word for line in list(np.concatenate((headlines, bodies), axis=None)) for word in line]
+
+        # Getting rid of duplicates
+        vocabulary = set(vocabulary)
+
+        # Making headlines and bodies strings
+        headlines = [" ".join(line) for line in headlines]
+        bodies = [" ".join(line) for line in bodies]
+
+        vectorizer = TfidfVectorizer(vocabulary=vocabulary)
+        headline_TF_IDF = vectorizer.fit_transform(headlines)
+        body_TF_IDF = vectorizer.fit_transform(bodies)
+
+        return sklearn.metrics.pairwise.cosine_similarity(headline_TF_IDF, body_TF_IDF)
+
+    def create_feature_file(self, path):
+        features = self.get_feature()
+
+        with open(path, 'wb') as f:
+            pickle.dump(features, f)
+
+    def read(self, name="train"):
+        self.name = name
+        if name == 'train':
+            if not os.path.exists(COSINE_SIMILARITY_FILE):
+                self.create_feature_file(COSINE_SIMILARITY_FILE)
+            return np.array(pickle.load(open(COSINE_SIMILARITY_FILE, 'rb'))).reshape(-1, 1)
+        else:
+            if not os.path.exists(COSINE_SIMILARITY_TEST_FILE):
+                self.create_feature_file(COSINE_SIMILARITY_TEST_FILE)
+            return np.array(pickle.load(open(COSINE_SIMILARITY_TEST_FILE, 'rb'))).reshape(-1, 1)
+
+
+
+sd = SVD()
+test = sd.get_feature()
+print(len(test))
